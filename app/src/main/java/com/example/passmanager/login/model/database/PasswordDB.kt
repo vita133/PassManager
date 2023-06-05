e7b3cf065609b164bbc8cf66290b6979a169a79a
Hrystyna Yaskulska <yaskulskahrystyna@gmail.com>
2023-06-05 21:18:33 +0300

    Add database for generated passwords store

---
+++ app/src/main/java/com/example/passmanager/login/model/database/PasswordDB.kt
@@ -0,0 +1,24 @@
+package com.example.passmanager.login.model.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.passmanager.login.model.entities.LoginEntity
+import com.example.passmanager.login.model.entities.PasswordEntity
+
+@Database(entities = [PasswordEntity::class], version = 1)
+abstract class PasswordDB : RoomDatabase() {
+    abstract fun getDao(): PasswordDao
+
+    companion object {
+        fun getLoginDB(context : Context) : PasswordDB{
+            return Room.databaseBuilder(
+                context.applicationContext,
+                PasswordDB::class.java,
+                "Passwords.db"
+            ).build()
+        }
+    }
+
+}
