6a1a2afe2c84f532d14c6adbc860bf5d5363b853
vita133 <vitandrukhiv@gmail.com>
2023-06-06 10:04:34 +0300

    add session token generation and storage in LoginActivity

--- app/src/main/java/com/example/passmanager/login/screens/LoginActivity.kt
+++ app/src/main/java/com/example/passmanager/login/screens/LoginActivity.kt
@@ -8,6 +8,8 @@ import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.example.passmanager.R
+import com.example.passmanager.login.util.SessionManagerUtil
+import java.util.*
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var username: EditText
@@ -36,6 +38,10 @@ class LoginActivity : AppCompatActivity() {
                 userViewModel.getUserByUsernameAndPassword(user, pass)
                 userViewModel.userByUsernameAndPasswordResult.observe(this@LoginActivity) { loggedInUser ->
                     if (loggedInUser != null) {
+                        val sessionToken = UUID.randomUUID().toString()
+                        SessionManagerUtil.storeUserToken(applicationContext, sessionToken)
+                        val expiresIn = 3600
+                        SessionManagerUtil.startUserSession(applicationContext, expiresIn)
                         Toast.makeText(this@LoginActivity, "Sign in successful", Toast.LENGTH_SHORT).show()
                         val intent = Intent(applicationContext, MainActivity::class.java).apply {
                             flags = Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK
